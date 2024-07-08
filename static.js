20a1c5f38d57797bde5f5a4c86c181b3cadb4d62
arpan khare <arpankhare17@gmail.com>
2024-07-08 22:37:26 +0530

    static

--- static.js
+++ static.js
@@ -1,3 +1,12 @@
+/* 
+Static properties cannot be directly accessed on instances of the class.
+Instead, they're accessed on the class itself.
+Static methods are often utility functions, 
+such as functions to create or clone objects, 
+whereas static properties are useful for caches, 
+fixed-configuration, or any other data you don't need to be replicated across instances.
+*/
+
 class MyClass {
   static #instance = null; // private static property
 
@@ -15,3 +24,32 @@ var instanceOne = new MyClass();
 var instanceTwo = new MyClass();
 
 console.log(instanceOne === instanceTwo); // Logs "true"
+
+// Static methods can be used to create instances of the class in a specific manner.
+class MyClass {
+  constructor(name) {
+    this.name = name;
+  }
+
+  static createWithDefaultName() {
+    return new MyClass("Default Name");
+  }
+}
+
+const obj = MyClass.createWithDefaultName();
+console.log(obj.name); // Outputs: 'Default Name'
+
+// Static methods can be used to clone an existing instance of the class.
+class MyClass {
+  constructor(name) {
+    this.name = name;
+  }
+
+  static cloneInstance(instance) {
+    return new MyClass(instance.name);
+  }
+}
+
+const original = new MyClass("Original");
+const clone = MyClass.cloneInstance(original);
+console.log(clone.name); // Outputs: 'Original'
