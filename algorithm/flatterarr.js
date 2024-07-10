59e2139adbf67eba212cbbb6d40bb53cd7386f94
arpan khare <arpankhare17@gmail.com>
2024-07-10 10:37:55 +0530

    Flatten arr concise solution

--- algorithm/flatterarr.js
+++ algorithm/flatterarr.js
@@ -1,4 +1,4 @@
-export default function flatten(value) {
+function flattenOne(value) {
   let flattenArr = [];
 
   function flattenUtil(currArr) {
@@ -14,3 +14,15 @@ export default function flatten(value) {
   flattenUtil(value);
   return flattenArr;
 }
+
+function flatten(arr) {
+  // acc.push doesn't work because
+  // push() adds elements to the end of an array and returns the new length of the array
+  // The concat() method is used to merge arrays. Concat does not change the existing arrays, but instead returns a new array.
+  return arr.reduce(
+    (acc, val) => acc.concat(Array.isArray(val) ? flatten(val) : val),
+    []
+  );
+}
+
+console.log(flatten([2, [3, 5], [5, 3, [2]]]));
