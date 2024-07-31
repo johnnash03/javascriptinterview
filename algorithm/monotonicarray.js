104e65d989d785be2ced2477d22637f64d593b81
arpan khare <arpankhare17@gmail.com>
2024-07-31 18:30:37 +0530

    monotonic array change

--- algorithm/monotonicarray.js
+++ algorithm/monotonicarray.js
@@ -1,7 +1,7 @@
 function isMonotonic(array) {
   let isIncreasing = true;
   let isDecreasing = true;
-  for (let i = 0; i < array.length - 2; i++) {
+  for (let i = 0; i < array.length - 1; i++) {
     if (array[i] > array[i + 1]) isIncreasing = false;
     if (array[i] < array[i + 1]) isDecreasing = false;
   }
