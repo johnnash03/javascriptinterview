9498358ce4b4ca2b88170e6af63c734310551fb5
arpan khare <arpankhare17@gmail.com>
2024-08-06 15:59:47 +0530

    state history hook

---
+++ statewithhistoryhook.js
@@ -0,0 +1,30 @@
+import { useState, useRef } from "react";
+
+function useStateWithHistory(initialState) {
+  const [state, setStateInternal] = useState(initalState);
+  const history = useRef([state]);
+
+  const historyIndex = useRef(0);
+
+  const setState = (newState) => {
+    history.current.push(newState);
+    history.current = history.current.length = 1;
+    setStateInternal(newState);
+  };
+
+  const goBack = () => {
+    if (historyIndex.current === 0) return;
+
+    historyIndex.current--;
+    setStateInternal(history.current[historyIndex.current]);
+  };
+
+  const goForward = () => {
+    if (historyIndex.current >= history.current.length - 1) return;
+
+    historyIndex.current++;
+    setStateInternal(history.current[historyIndex.current]);
+  };
+
+  return [state, setState, goBack, goForward, history.current];
+}
